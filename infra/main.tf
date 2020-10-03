d368a8fe17918aed38c9c33a97eeb5b4e308b20f
Bart Lammers <bart.f.lammers@gmail.com>
2020-10-03 20:46:52 +0200

    Updated directory structure, removed separate infra folder

--- infra/main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  version = "~> 2.0"
-  region  = "eu-west-2" # Setting my region to London. Use your own region here
-}
-
-resource "aws_ecr_repository" "scalable_web_app_repo" {
-  name = "scalable_web_app" # Naming my repository
-}
