2676a2c56e489e498085787cb1054ce050a204d5
Bart Lammers <bart.f.lammers@gmail.com>
2020-10-11 19:57:28 +0200

    Working on EB

--- infra/app/main.tf
+++ infra/app/main.tf
@@ -1,25 +1,25 @@
 
 provider "aws" {
-  region = "eu-west-1"
+  region = var.region
 }
 
 module "vpc" {
   source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=master"
-  namespace  = "us"
-  stage      = "dev"
-  name       = "dev-vpc"
+  namespace  = var.namespace
+  stage      = var.stage
+  name       = var.name
   cidr_block = "172.16.0.0/16"
 }
 
 module "subnets" {
-  source               = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=master"
-  namespace            = "us"
-  stage                = "dev"
-  name                 = "dev-vpc"
-  vpc_id               = module.vpc.vpc_id
-  igw_id               = module.vpc.igw_id
-  cidr_block           = module.vpc.vpc_cidr_block
-  availability_zones   = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  source             = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=master"
+  namespace          = var.namespace
+  stage              = var.stage
+  name               = var.name
+  vpc_id             = module.vpc.vpc_id
+  igw_id             = module.vpc.igw_id
+  cidr_block         = module.vpc.vpc_cidr_block
+  availability_zones = var.availability_zones
 
   nat_gateway_enabled  = true
   nat_instance_enabled = false
@@ -27,23 +27,23 @@ module "subnets" {
 
 module "elastic_beanstalk_application" {
   source      = "git::https://github.com/cloudposse/terraform-aws-elastic-beanstalk-application.git?ref=master"
-  namespace   = "us"
-  stage       = "dev"
-  name        = "dev-vpc"
+  namespace   = var.namespace
+  stage       = var.stage
+  name        = var.name
   description = "Test elastic_beanstalk_application"
 }
 
 module "elastic_beanstalk_environment" {
   source                             = "git::https://github.com/cloudposse/terraform-aws-elastic-beanstalk-environment.git?ref=master"
-  namespace                          = "us"
-  stage                              = "dev"
-  name                               = "dev-vpc"
+  namespace                          = var.namespace
+  stage                              = var.stage
+  name                               = var.name
   description                        = "Test elastic_beanstalk_environment"
-  region                             = "eu-west-1"
+  region                             = var.region
   availability_zone_selector         = "Any 2"
   elastic_beanstalk_application_name = module.elastic_beanstalk_application.elastic_beanstalk_application_name
 
-  instance_type           = "t3.small"
+  instance_type           = "t3.micro"
   autoscale_min           = 1
   autoscale_max           = 2
   updating_min_in_service = 0
@@ -57,7 +57,7 @@ module "elastic_beanstalk_environment" {
 
   // https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html
   // https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html#platforms-supported.docker
-  solution_stack_name = "64bit Amazon Linux 2 v3.1.2 running Docker"
+  solution_stack_name = "64bit Amazon Linux 2018.03 v2.16.0 running Docker 19.03.6-ce"
 
   additional_settings = [
     {
---
+++ infra/app/variables.tf
@@ -0,0 +1,22 @@
+
+variable "region" {
+    type    = string
+    default = "eu-west-1"
+}
+
+variable "availability_zones" {
+    type    = list(string)
+    default = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+}
+
+variable "namespace" {
+    type    = string
+}
+
+variable "stage" {
+    type    = string
+}
+
+variable "name" {
+    type    = string
+}
