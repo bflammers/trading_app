8d00d3638152a8218d91c11d9e7aa3eeb9be2c53
Bart Lammers <bart.f.lammers@gmail.com>
2020-11-01 22:31:42 +0100

    Started connecting db to vpc

--- infra/01_vpc/output.tf
+++ infra/01_vpc/output.tf
@@ -10,3 +10,141 @@ output "id_full" {
   value       = module.label.enabled ? module.label.id_full : ""
   description = "Disambiguated ID not restricted in length"
 }
+
+# VPC
+
+output "database_internet_gateway_route_id" {
+  value = module.vpc.database_internet_gateway_route_id
+}
+
+output "database_subnets_ids" {
+  value = module.vpc.database_subnets
+}
+
+output "database_subnets" {
+  value = module.vpc.database_subnets
+}
+
+output "database_subnet_group" {
+  value = module.vpc.database_subnet_group
+}
+
+output "database_subnet_arns" {
+  value = module.vpc.database_subnet_arns
+}
+
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "default_vpc_arn" {
+  value = module.vpc.default_vpc_id
+}
+
+output "default_vpc_id" {
+  value = module.vpc.default_vpc_id
+}
+
+output "default_vpc_main_route_table_id" {
+  value = module.vpc.default_vpc_main_route_table_id
+}
+
+output "default_vpc_default_route_table_id" {
+  value = module.vpc.default_vpc_default_route_table_id
+}
+
+output "vpc_endpoint_elasticbeanstalk_id" {
+  value = module.vpc.vpc_endpoint_elasticbeanstalk_id
+}
+
+output "vpc_endpoint_elasticbeanstalk_network_interface_ids" {
+  value = module.vpc.vpc_endpoint_elasticbeanstalk_network_interface_ids
+}
+
+output "vpc_endpoint_elasticbeanstalk_dns_entry" {
+  value = module.vpc.vpc_endpoint_elasticbeanstalk_dns_entry
+}
+
+output "vpc_endpoint_ec2_id" {
+  value = module.vpc.vpc_endpoint_ec2_id
+}
+
+output "vpc_endpoint_ec2_dns_entry" {
+  value = module.vpc.vpc_endpoint_ec2_dns_entry
+}
+
+output "vpc_endpoint_ec2_network_interface_ids" {
+  value = module.vpc.vpc_endpoint_ec2_network_interface_ids
+}
+
+output "vpc_arn" {
+  value = module.vpc.vpc_arn
+}
+
+output "vgw_id" {
+  value = module.vpc.vgw_id
+  description = "The ID of the VPN Gateway"
+}
+
+output "vgw_arn" {
+  value = module.vpc.vgw_arn
+  description = "The arn of the VPN Gateway"
+}
+
+output "public_subnets" {
+  value = module.vpc.public_subnets
+}
+
+output "public_subnet_arns" {
+  value = module.vpc.public_subnet_arns
+}
+
+output "public_subnets_cidr_blocks" {
+  value = module.vpc.public_subnets_cidr_blocks
+}
+
+output "private_subnets" {
+  value = module.vpc.private_subnets
+}
+
+output "private_subnet_arns" {
+  value = module.vpc.private_subnet_arns
+}
+
+output "private_subnets_cidr_blocks" {
+  value = module.vpc.private_subnets_cidr_blocks	
+}
+
+output "public_internet_gateway_route_id" {
+  value = module.vpc.public_internet_gateway_route_id
+}
+
+output "natgw_ids" {
+  value = module.vpc.natgw_ids
+}
+
+output "nat_public_ips" {
+  value = module.vpc.nat_public_ips
+}
+
+output "nat_ids" {
+  value = module.vpc.nat_ids
+}
+
+output "name" {
+  value = module.vpc.name
+}
+
+output "igw_arn" {
+  value = module.vpc.igw_arn
+  description = "The ARN of the Internet Gateway"
+}
+
+output "igw_id" {
+  value = module.vpc.igw_id
+  description = "The ID of the Internet Gateway"
+}
+
+
+
+
--- infra/02_db/main.tf
+++ infra/02_db/main.tf
@@ -21,6 +21,20 @@ module "label" {
   name        = var.name
 }
 
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+
+  config = {
+    bucket  = "unicorn-shepherd-terraform-state"
+    key     = "dev/vpc/terraform.tfstate"
+    region  = "eu-west-1"
+  }
+}
+
+# output "test" {
+#   value = data.terraform_remote_state.vpc.outputs.id
+# }
+
 ##############################################################
 # Data sources to get VPC, subnets and security group details
 ##############################################################
@@ -30,70 +44,70 @@ module "label" {
 # }
 
 # data "aws_subnet_ids" "all" {
-#   vpc_id = data.aws_vpc.default.id
+#   vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
 # }
 
-# data "aws_security_group" "default" {
-#   vpc_id = data.aws_vpc.default.id
-#   name   = "default"
-# }
+data "aws_security_group" "default" {
+  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
+  name   = "default"
+}
 
-# #####
-# # DB
-# #####
-# module "db" {
-#   source = "terraform-aws-modules/rds/aws"
+#####
+# DB
+#####
+module "db" {
+  source = "terraform-aws-modules/rds/aws"
 
-#   identifier = "sql-postgres"
+  identifier = "sql-postgres"
 
-#   engine            = "postgres"
-#   engine_version    = "9.6.9"
-#   instance_class    = "db.t2.micro"
-#   allocated_storage = 5
-#   storage_encrypted = false
+  engine            = "postgres"
+  engine_version    = "9.6.9"
+  instance_class    = "db.t2.micro"
+  allocated_storage = 5
+  storage_encrypted = false
 
-#   # kms_key_id        = "arm:aws:kms:<region>:<account id>:key/<kms key id>"
-#   name = "postgres"
+  # kms_key_id        = "arm:aws:kms:<region>:<account id>:key/<kms key id>"
+  name = "postgres"
 
-#   # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
-#   # "Error creating DB Instance: InvalidParameterValue: MasterUsername
-#   # user cannot be used as it is a reserved word used by the engine"
-#   username = "postgres"
+  # NOTE: Do NOT use 'user' as the value for 'username' as it throws:
+  # "Error creating DB Instance: InvalidParameterValue: MasterUsername
+  # user cannot be used as it is a reserved word used by the engine"
+  username = "postgres"
 
-#   password = "postgres"
-#   port     = "5432"
+  password = "postgres"
+  port     = "5432"
 
-#   vpc_security_group_ids = [data.aws_security_group.default.id]
-#   publicly_accessible = true
+  vpc_security_group_ids = [data.aws_security_group.default.id]
+  publicly_accessible = true
 
-#   maintenance_window = "Mon:00:00-Mon:03:00"
-#   backup_window      = "03:00-06:00"
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  backup_window      = "03:00-06:00"
 
-#   # disable backups to create DB faster
-#   backup_retention_period = 0
+  # disable backups to create DB faster
+  backup_retention_period = 0
 
-#   tags = {
-#     Owner       = "user"
-#     Environment = "dev"
-#   }
+  tags = {
+    Owner       = "user"
+    Environment = "dev"
+  }
 
-#   enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
+  enabled_cloudwatch_logs_exports = ["postgresql", "upgrade"]
 
-#   # DB subnet group
-#   subnet_ids = data.aws_subnet_ids.all.ids
+  # DB subnet group
+  subnet_ids = data.terraform_remote_state.vpc.outputs.public_subnets
 
-#   # DB parameter group
-#   family = "postgres9.6"
+  # DB parameter group
+  family = "postgres9.6"
 
-#   # DB option group
-#   major_engine_version = "9.6"
+  # DB option group
+  major_engine_version = "9.6"
 
-#   # Snapshot name upon DB deletion
-#   final_snapshot_identifier = "demodb"
+  # Snapshot name upon DB deletion
+  final_snapshot_identifier = "demodb"
 
-#   # Database Deletion Protection
-#   deletion_protection = false
-# }
+  # Database Deletion Protection
+  deletion_protection = false
+}
 
 #
 # EC2
@@ -201,83 +215,3 @@ module "label" {
 #  }
 #
 #}
-#
-#
-# module "vpc" {
-#   source     = "git::https://github.com/cloudposse/terraform-aws-vpc.git?ref=master"
-#   namespace  = var.namespace
-#   stage      = var.stage
-#   name       = var.name
-#   cidr_block = "172.16.0.0/16"
-# }
-
-# module "subnets" {
-#   source             = "git::https://github.com/cloudposse/terraform-aws-dynamic-subnets.git?ref=master"
-#   namespace          = var.namespace
-#   stage              = var.stage
-#   name               = var.name
-#   vpc_id             = module.vpc.vpc_id
-#   igw_id             = module.vpc.igw_id
-#   cidr_block         = module.vpc.vpc_cidr_block
-#   availability_zones = var.availability_zones
-
-#   nat_gateway_enabled  = true
-#   nat_instance_enabled = false
-# }
-
-# module "elastic_beanstalk_application" {
-#   source      = "git::https://github.com/cloudposse/terraform-aws-elastic-beanstalk-application.git?ref=master"
-#   namespace   = var.namespace
-#   stage       = var.stage
-#   name        = var.name
-#   description = "Trading APP - elastic_beanstalk_application"
-# }
-
-# module "elastic_beanstalk_environment" {
-#   source                             = "git::https://github.com/cloudposse/terraform-aws-elastic-beanstalk-environment.git?ref=master"
-#   namespace                          = var.namespace
-#   stage                              = var.stage
-#   name                               = var.name
-#   description                        = "Trading APP - elastic_beanstalk_environment"
-#   region                             = var.region
-#   availability_zone_selector         = "Any 2"
-#   elastic_beanstalk_application_name = module.elastic_beanstalk_application.elastic_beanstalk_application_name
-
-#   instance_type           = "t3.small"
-#   autoscale_min           = 1
-#   autoscale_max           = 2
-#   updating_min_in_service = 0
-#   updating_max_batch      = 1
-
-#   loadbalancer_type       = "application"
-#   vpc_id                  = module.vpc.vpc_id
-#   loadbalancer_subnets    = module.subnets.public_subnet_ids
-#   application_subnets     = module.subnets.private_subnet_ids
-#   allowed_security_groups = [module.vpc.vpc_default_security_group_id]
-
-#   // https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html
-#   // https://docs.aws.amazon.com/elasticbeanstalk/latest/platforms/platforms-supported.html#platforms-supported.docker
-#   solution_stack_name = "64bit Amazon Linux 2018.03 v2.16.0 running Docker 19.03.6-ce"
-
-#   additional_settings = [
-#     {
-#       namespace = "aws:elasticbeanstalk:application:environment"
-#       name      = "ANOTHER_ENV_VAR"
-#       value     = "123456789"
-#     }
-#   ]
-# }
-
-# # Does not work as it should, for now configuring routing policy manually
-# resource "aws_route53_record" "domain_route" {
-#   zone_id = var.hosted_zone_id
-#   name    = "unicornshepherd.com"
-#   type    = "A"
-#   # ttl     = "300"
-#   # records = [module.elastic_beanstalk_environment.endpoint]
-#   alias {
-#     name = module.elastic_beanstalk_environment.endpoint
-#     zone_id = module.elastic_beanstalk_environment.elb_zone_id
-#     evaluate_target_health = true
-#   }
-# }
